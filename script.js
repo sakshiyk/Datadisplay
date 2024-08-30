9cf7ab3e601e01c83de9d87ba5669b1be5ecf9de
sakshiyk <skulkarn2@gitam.in>
2024-08-30 21:03:44 +0530

    completed

--- script.js
+++ script.js
@@ -4,27 +4,26 @@ entry.addEventListener("click", displayDetails);
 var row = 1;
 
 function displayDetails() {
-    var Name=document.getElementById("Name").value;
-    var Age=document.getElementById("Age").value;
-    var Grade=document.getElementById("Grade").value;
+    var Name = document.getElementById("Name").value;
+    var Age = document.getElementById("Age").value;
+    var Grade = document.getElementById("Grade").value;
 
-    if(!Name || !Age || !Grade){
+    if (!Name || !Age || !Grade) {
         alert("Please fill all the above boxes");
         return;
     }
-    var display =document.getElementById("display");
+    
+    var display = document.getElementById("display");
 
-    var newRow = display.insertROW(row);
+    var newRow = display.insertRow(row);
 
-    var Cell1 = newRow.insertCell(0);
-    var Cell2 = newRow.insertCell(1);
-    var Cell3 = newRow.insertCell(2);
+    var cell1 = newRow.insertCell(0);
+    var cell2 = newRow.insertCell(1);
+    var cell3 = newRow.insertCell(2);
 
     cell1.innerHTML = Name;
     cell2.innerHTML = Age;
-    Cell3.innerHTML = Grade;
+    cell3.innerHTML = Grade;
 
-    row++;
-
-
+    row++;
 }
